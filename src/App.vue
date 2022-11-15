e08003b71b9fa31e65b8ee00f5c3bdd83322a588
wzt <2238091763@qq.com>
2022-11-15 23:44:14 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,81 @@
 <template>
-  <Header title="购物车"></Header>
+  <div class="app-container">
+    <Header title="购物车"></Header>
+    <Goods
+        v-for="item in list"
+        :key="item.id"
+        :id="item.id"
+        :title="item.goods_name"
+        :pic="item.goods_img"
+        :price="item.goods_price"
+        :state="item.goods_state"
+        :count="item.goods_count"
+        @state-change="getNewState"
+    ></Goods>
+    <Footer :isfull="fullState" @full-change="getFullState"></Footer>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 import Header from '@/components/HeaderCom.vue'
+import Goods from '@/components/GoodsCom.vue'
+import Footer from '@/components/FooterCom.vue'
 
 export default {
+  data() {
+    return {
+      list: []
+    }
+  },
+  computed:{
+    fullState() {
+      return this.list.every(item => item.goods_state)
+    },
+  },
+  methods: {
+    // 封装请求列表数据的方法
+    async initCartList() {
+      // 调用 axios 的 get 方法，请求列表数据
+      const {data: res} = await axios.get('https://www.escook.cn/api/cart')
+      // 只要请求回来的数据，在页面渲染期间要用到，则必须转存到 data 中
+      if (res.status === 200) {
+        console.log(res)
+        this.list = res.list
+      }
+    },
+    getNewState(e) {
+      this.list.some((item, index) => {
+        if (item.id === e.id) {
+          this.list[index].goods_state = e.value
+          return true
+        }
+      })
+    },
+    getFullState(val) {
+      this.list.forEach(item => (item.goods_state = val))
+    }
+  },
+
   name: 'App',
   components: {
-    Header
+    Header,
+    Goods,
+    Footer
+  },
+  created() {
+    this.initCartList()
   }
 }
 </script>
 
 <style>
-* {
+body {
   margin: 0;
+}
+
+.app-container {
+  padding-top: 45px;
+  padding-bottom: 50px;
 }
 </style>
---
+++ src/components/FooterCom.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="footer-container">
+    <!-- 左侧的全选 -->
+    <div class="custom-control custom-checkbox">
+      <input type="checkbox" class="custom-control-input" id="cbFull" :checked="isfull" @change="fullChange"/>
+      <label class="custom-control-label" for="cbFull">全选</label>
+    </div>
+
+    <!-- 中间的合计 -->
+    <div>
+      <span>合计：</span>
+      <span class="total-price">￥{{ 0 }}</span>
+    </div>
+
+    <!-- 结算按钮 -->
+    <button type="button" class="btn btn-primary btn-settle">结算（{{ 0 }}）</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    // 监听到了全选的状态变化
+    fullChange(e) {
+      this.$emit('full-change', e.target.checked)
+    }
+  },
+  props: {
+    isfull: {
+      type: Boolean,
+      default: true
+    },
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.footer-container {
+  font-size: 12px;
+  height: 50px;
+  width: 100%;
+  border-top: 1px solid #efefef;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.custom-checkbox {
+  margin-left: 10px;
+  display: flex;
+  align-items: center;
+}
+
+#cbFull {
+  margin-right: 5px;
+}
+
+.btn-settle {
+  margin-right: 10px;
+  height: 80%;
+  min-width: 110px;
+  border-radius: 25px;
+  font-size: 12px;
+}
+
+.total-price {
+  font-weight: bold;
+  font-size: 14px;
+  color: red;
+}
+</style>
